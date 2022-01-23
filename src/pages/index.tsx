75bf74520c578ac85f36a6704344940113e53002
Anderson Soares <anderson.ceu@gmail.com>
2022-01-23 10:58:07 -0400

    setting up tailwindcss and re-structuring folders

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-center text-5xl font-bold">Hello World</h1>
+    </div>
+  );
+};
+
+export default Home;
